56e95fdb6e73ff2922a443f4b36da722c514cccd
shimiwaka <kei_ikeda@r.recruit.co.jp>
2023-06-25 17:24:17 +0900

    apply format

--- cmd/sample/main.go
+++ cmd/sample/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "bytes"
+	"bytes"
 	"fmt"
 
 	"github.com/shimiwaka/str2img"
@@ -10,10 +10,10 @@ import (
 func main() {
 	generater := &str2img.Generater{
 		ImageHeight: 630,
-		ImageWidth: 1200,
-		FontSize: 40.0,
-		FontFile: "Koruri-Regular.ttf",
-		ImageBytes: &bytes.Buffer{},
+		ImageWidth:  1200,
+		FontSize:    40.0,
+		FontFile:    "Koruri-Regular.ttf",
+		ImageBytes:  &bytes.Buffer{},
 	}
 
 	err := generater.Generate("テストてすとです\nほげほげほげ太郎\nほげ太郎")
--- str2img.go
+++ str2img.go
@@ -1,35 +1,35 @@
 package str2img
 
 import (
-    "bytes"
-    "image"
-    "image/png"
-    "os"
+	"bytes"
+	"image"
+	"image/color"
+	"image/png"
 	"io/ioutil"
-    "image/color"
+	"os"
 	"strings"
 
-    "github.com/golang/freetype/truetype"
-    "golang.org/x/image/font"
-    "golang.org/x/image/math/fixed"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
 )
 
-type Generater struct  {
-	ImageHeight int;
-	ImageWidth int;
-	FontSize float64;
-	FontFile string;
-	ImageBytes *bytes.Buffer;
+type Generater struct {
+	ImageHeight int
+	ImageWidth  int
+	FontSize    float64
+	FontFile    string
+	ImageBytes  *bytes.Buffer
 }
 
 func fillRect(img *image.RGBA, col color.Color) {
-    rect := img.Rect
+	rect := img.Rect
 
-    for h := rect.Min.Y; h < rect.Max.Y; h++ {
-        for v := rect.Min.X; v < rect.Max.X; v++ {
-            img.Set(v, h, col)
-        }
-    }
+	for h := rect.Min.Y; h < rect.Max.Y; h++ {
+		for v := rect.Min.X; v < rect.Max.X; v++ {
+			img.Set(v, h, col)
+		}
+	}
 }
 
 func textSplitter(dr *font.Drawer, fontSize float64, imageHeight int, imageWidth int, texts []string) []string {
@@ -42,9 +42,9 @@ func textSplitter(dr *font.Drawer, fontSize float64, imageHeight int, imageWidth
 			if splitLen < 1 {
 				splitLen = 1
 			}
-		 	for i := 0; i < len(v); i += splitLen {
-				if i + splitLen < len(v) {
-					newTexts = append(newTexts, v[i:(i + splitLen)])
+			for i := 0; i < len(v); i += splitLen {
+				if i+splitLen < len(v) {
+					newTexts = append(newTexts, v[i:(i+splitLen)])
 				} else {
 					newTexts = append(newTexts, v[i:])
 				}
@@ -61,69 +61,69 @@ func textSplitter(dr *font.Drawer, fontSize float64, imageHeight int, imageWidth
 }
 
 func (g *Generater) Generate(rawText string) error {
-    ftBinary, err := ioutil.ReadFile(g.FontFile)
-    if err != nil {
-        return err
-    }
+	ftBinary, err := ioutil.ReadFile(g.FontFile)
+	if err != nil {
+		return err
+	}
 
-    ft, err := truetype.Parse(ftBinary)
-    if err != nil {
-        return err
-    }
+	ft, err := truetype.Parse(ftBinary)
+	if err != nil {
+		return err
+	}
 	fontSize := g.FontSize
 
-    opt := truetype.Options{
-        Size:              fontSize,
-        DPI:               0,
-        Hinting:           0,
-        GlyphCacheEntries: 0,
-        SubPixelsX:        0,
-        SubPixelsY:        0,
-    }
+	opt := truetype.Options{
+		Size:              fontSize,
+		DPI:               0,
+		Hinting:           0,
+		GlyphCacheEntries: 0,
+		SubPixelsX:        0,
+		SubPixelsY:        0,
+	}
 
-    imageWidth := g.ImageWidth
-    imageHeight := g.ImageHeight
+	imageWidth := g.ImageWidth
+	imageHeight := g.ImageHeight
 
 	texts := strings.Split(rawText, "\n")
 
-    img := image.NewRGBA(image.Rect(0, 0, imageWidth, imageHeight))
-    fillRect(img, color.RGBA{255, 255, 255, 255})
+	img := image.NewRGBA(image.Rect(0, 0, imageWidth, imageHeight))
+	fillRect(img, color.RGBA{255, 255, 255, 255})
 
-    face := truetype.NewFace(ft, &opt)
+	face := truetype.NewFace(ft, &opt)
 
-    dr := &font.Drawer{
-        Dst:  img,
-        Src:  image.Black,
-        Face: face,
-        Dot:  fixed.Point26_6{},
-    }
+	dr := &font.Drawer{
+		Dst:  img,
+		Src:  image.Black,
+		Face: face,
+		Dot:  fixed.Point26_6{},
+	}
 
 	texts = textSplitter(dr, fontSize, imageHeight, imageWidth, texts)
 	textTopMargin := (imageHeight - ((len(texts) - 1) * (int(fontSize) + 10))) / 2
 
 	for i, v := range texts {
 		dr.Dot.X = (fixed.I(imageWidth) - dr.MeasureString(v)) / 2
-		dr.Dot.Y = fixed.I(textTopMargin + i * (int(fontSize) + 10))
+		dr.Dot.Y = fixed.I(textTopMargin + i*(int(fontSize)+10))
 
 		dr.DrawString(v)
 	}
 
-    g.ImageBytes = &bytes.Buffer{}
-    err = png.Encode(g.ImageBytes, img)
+	g.ImageBytes = &bytes.Buffer{}
+	err = png.Encode(g.ImageBytes, img)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (g *Generater) OutputImageFile(fileName string) error {
-    file, err := os.Create(fileName)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-    file.Write(g.ImageBytes.Bytes())
+	file.Write(g.ImageBytes.Bytes())
 	return nil
 }
